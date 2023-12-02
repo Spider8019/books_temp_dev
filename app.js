15208ad3c22f52417bac6739764c75e5a2be11eb
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2023-12-02 23:57:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql2')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const pool = mysql.createPool({
+  host: 'bxvdqypaoapk8gq6b7vt-mysql.services.clever-cloud.com',
+  user: 'utwqxdx7kw0luqml',
+  password: 'utwqxdx7kw0luqml',
+  database: 'bxvdqypaoapk8gq6b7vt',
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/getAllBooks', async (req, res) => {
+  console.log(req.query)
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to MySQL database: ', err)
+      // Handle the error
+      return
+    }
+
+    connection.query(`SELECT * FROM BOOK`, (err, results) => {
+      if (err) {
+        console.error('Error executing query: ', err)
+        return
+      }
+
+      res.send(results)
+    })
+
+    // Use the connection for executing queries
+    console.log('connected')
+
+    // Release the connection when finished
+    connection.release()
+  })
+})
+
+app.listen(8000, () => [console.log('server started running')])
