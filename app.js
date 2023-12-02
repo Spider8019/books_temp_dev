229a74acd7ece882306c77a9c70b0d14d1799644
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2023-12-03 00:04:54 +0530

    asdfa

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/getAllBooks', async (req, res) => {
+app.get('/', async (req, res) => {
   console.log(req.query)
   pool.getConnection((err, connection) => {
     if (err) {
