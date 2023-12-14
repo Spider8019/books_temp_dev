7c9f5b479630517e3308b4e77f8f783f61c7f4fa
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2023-12-14 11:35:41 +0530

    frew more apis

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  // host: 'localhost',
+  // user: 'root',
+  // password: 'Aman@123',
+  // database: 'books_temp',
 })
 
 app.use(cors())
@@ -34,6 +38,100 @@ app.get('/novels/', async (req, res) => {
 
       res.send(results)
     })
+
+    // Use the connection for executing queries
+    console.log('connected')
+
+    // Release the connection when finished
+    connection.release()
+  })
+})
+
+app.get('/novel/:title', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to MySQL database: ', err)
+      // Handle the error
+      return
+    }
+
+    connection.query(
+      `SELECT * FROM books WHERE title like "%${req.params.title}%"`,
+      (err, results) => {
+        if (err) {
+          console.error('Error executing query: ', err)
+          return res.status(500).json({
+            error: 'Internal Server Error',
+            message: err.message,
+          })
+        } else if (results.length == 0)
+          return res.status(404).json({
+            error: 'Not found',
+          })
+        res.send(results)
+      },
+    )
+
+    // Use the connection for executing queries
+    console.log('connected')
+
+    // Release the connection when finished
+    connection.release()
+  })
+})
+
+app.post('/createcustomer/', async (req, res) => {
+  console.log('request arrived', req.body)
+  const regexPattern = /^91\d{10}@c\.us$/
+  if (regexPattern.test(req.body.phonenumber)) {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.error('Error connecting to MySQL database: ', err)
+        return
+      }
+
+      connection.query(
+        `insert into users (wallet_balance, name, phonenumber) values (0,"","${req.body.phonenumber}")`,
+        (err, results) => {
+          if (err) {
+            console.error('Error executing query: ', err)
+            return res.status(500).json({
+              error: 'Internal Server Error',
+              message: err.message,
+            })
+          }
+          res.send(results)
+        },
+      )
+      console.log('connected')
+
+      // Release the connection when finished
+      connection.release()
+    })
+  }
+})
+
+app.get('/userdetail/:phonenumber', async (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to MySQL database: ', err)
+      // Handle the error
+      return
+    }
+
+    connection.query(
+      `SELECT * FROM users WHERE phonenumber = "${req.params.phonenumber}"`,
+      (err, results) => {
+        if (err) {
+          console.error('Error executing query: ', err)
+          return res.status(500).json({
+            error: 'Internal Server Error',
+            message: err.message,
+          })
+        }
+        res.send(results)
+      },
+    )
 
     // Use the connection for executing queries
     console.log('connected')
